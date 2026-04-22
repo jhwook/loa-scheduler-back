ff97978d556656ccf6f78a2fb8e6c339dfdc2289
jhwook <jhwooook@naver.com>
2026-04-22 13:36:43 +0900

    .

--- src/character-weekly-raid/character-weekly-raid-gate.controller.ts
+++ src/character-weekly-raid/character-weekly-raid-gate.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { CharacterWeeklyRaidGateService } from './character-weekly-raid-gate.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'src/auth/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth-guard';
 import { RolesGuard } from 'src/auth/roles.guard';
 
+@ApiTags('Character-weekly-raid-gates')
+@ApiBearerAuth()
 @Controller('character-weekly-raid-gates')
 export class CharacterWeeklyRaidGateController {
   constructor(
--- src/character-weekly-raid/character-weekly-raid-gate.module.ts
+++ src/character-weekly-raid/character-weekly-raid-gate.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CharacterWeeklyRaidGate } from './entities/character-weekly-raid-gate.entity';
 import { CharacterWeeklyRaidGateService } from './character-weekly-raid-gate.service';
 import { RaidGateInfo } from 'src/raid-info/entities/raid-gate-info.entity';
+import { CharacterWeeklyRaidGateController } from './character-weekly-raid-gate.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CharacterWeeklyRaidGate, RaidGateInfo])],
+  controllers: [CharacterWeeklyRaidGateController],
   providers: [CharacterWeeklyRaidGateService],
   exports: [CharacterWeeklyRaidGateService],
 })
